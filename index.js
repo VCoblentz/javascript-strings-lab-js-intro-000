8a054b4be400ff91a836daedd1eba5bfe694e9e6
Vance Coblentz <vancecoblentz@yahoo.com>
2019-12-05 18:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space.";
+var topic = "space";
 
-var conversation = `let\'s talk about ${topic}`;
+var conversation = `let\'s talk about ${topic}.`;
